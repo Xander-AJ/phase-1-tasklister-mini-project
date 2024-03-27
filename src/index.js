0859dc9ac16d5231f5f8eb967c6d7d5a094b7c41
John-Alexander Kamau <xander.kamau7@gmail.com>
2024-03-27 16:24:08 +0300

    complete lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+    const form = document.getElementById("create-task-form");
+    const taskList = document.getElementById("tasks");
+  
+    form.addEventListener("submit", (event) => {
+      event.preventDefault(); // Prevent default form submission
+  
+      const inputField = document.getElementById("new-task-description");
+      const task = inputField.value;
+  
+      const newTaskItem = document.createElement("li");
+      newTaskItem.textContent = task;
+      taskList.appendChild(newTaskItem);
+  
+      inputField.value = ""; // Clear the input field after submitting
+    });
+  });
+  
+
